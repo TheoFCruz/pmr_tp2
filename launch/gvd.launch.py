bf891ff92d8873cbb60426ff343766c2523c7aae
TheoFCruz <theofonsecacruz@gmail.com>
2026-05-28 08:28:53 -0300

    gvd launch

---
+++ launch/gvd.launch.py
@@ -0,0 +1,77 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    # Packages
+    pmr_tp2_pkg = FindPackageShare('pmr_tp2')
+
+    # Filepaths
+    default_rviz_config_path = PathJoinSubstitution([pmr_tp2_pkg, 'rviz', 'gvd.rviz'])
+
+    # Arguments
+    world_name = LaunchConfiguration('world')
+    map_name = LaunchConfiguration('map_name')
+    rviz_config_path = LaunchConfiguration('rviz_config_path')
+
+    declare_world = DeclareLaunchArgument(
+        'world',
+        default_value='empty.sdf',
+        description='Gazebo world name'
+    )
+
+    declare_map_name = DeclareLaunchArgument(
+        'map_name',
+        default_value='gvd_map.yaml',
+        description='Map YAML file name'
+    )
+
+    declare_rviz_config_path = DeclareLaunchArgument(
+        'rviz_config_path',
+        default_value=default_rviz_config_path,
+        description='Path to the RViz configuration file'
+    )
+
+    # Includes
+    sim_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([pmr_tp2_pkg, 'launch', 'include', 'sim_create3.launch.py'])
+        ),
+        launch_arguments={'world': world_name}.items()
+    )
+
+    rviz_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([pmr_tp2_pkg, 'launch', 'include', 'rviz_map.launch.py'])
+        ),
+        launch_arguments={
+            'map_name': map_name,
+            'rviz_config_path': rviz_config_path
+        }.items()
+    )
+
+    # GVD Node
+    gvd_node = Node(
+        package='pmr_tp2',
+        executable='gvd',
+        name='gvd',
+        output='screen',
+        parameters=[{'use_sim_time': True}]
+    )
+
+    ld = LaunchDescription()
+
+    # Add arguments
+    ld.add_action(declare_world)
+    ld.add_action(declare_map_name)
+    ld.add_action(declare_rviz_config_path)
+
+    # Add actions
+    ld.add_action(sim_launch)
+    ld.add_action(rviz_launch)
+    ld.add_action(gvd_node)
+
+    return ld
